5233b3c4aba9939bc01c37f7a4bbed3a0513b4d8
mihawkk-29 <185799778+mihawkk-29@users.noreply.github.com>
2025-08-09 23:33:04 +0530

    Update Main.java
    A option to choose the opponent is given

--- src/com/tictactoe/Main.java
+++ src/com/tictactoe/Main.java
@@ -10,18 +10,36 @@ import com.tictactoe.strategy.ColWinningStrategy;
 import com.tictactoe.strategy.RowWinningStrategy;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Game Mode");
+        System.out.println("1.multiplayer" +
+                "2.computer");
+        int n = s.nextInt();
+        Player aarunPlayer = null;
         User edwin = new User("Edwin", "edwin@xyz");
         Player edwinPlayer = new HumanPlayer(edwin, 'X');
+        switch (n) {
+            case 1 -> {
 
-        User bot = new User("Bot", "bot@xyz.com");
-        Player botPlayer = new BotPlayer(bot, 'O', BotDifficultyLevel.EASY);
+                User aarun = new User("Aarun", "ajk@xyz.com");
+                aarunPlayer = new HumanPlayer(aarun, 'O');
+            }
+            case 2 -> {
 
-        new GameController(3,
-                List.of(edwinPlayer, botPlayer),
-                List.of(new ColWinningStrategy(), new RowWinningStrategy())
-        ).playGame();
+                User bot = new User("Bot", "bot@xyz.com");
+                aarunPlayer = new BotPlayer(bot, 'O', BotDifficultyLevel.EASY);
+            }
+        }
+
+
+            new GameController(3,
+                    List.of(edwinPlayer, aarunPlayer),
+                    List.of(new ColWinningStrategy(), new RowWinningStrategy())
+            ).playGame();
+        }
+
     }
-}
